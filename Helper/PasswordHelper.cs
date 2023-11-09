e6ca93719148eaa36451f86ee333bc8b134dee71
Andy <phanvietthang94@gmail.com>
2023-11-10 00:09:03 +0700

    feat: create login UI

---
+++ Helper/PasswordHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PosApp.Helper
+{
+    public static class PasswordHelper
+    {
+        public static readonly DependencyProperty AttachProperty =
+         DependencyProperty.RegisterAttached("Attach", typeof(bool), typeof(PasswordHelper), new PropertyMetadata(false, AttachPropertyChanged));
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password", typeof(string), typeof(PasswordHelper));
+
+        public static bool GetAttach(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AttachProperty);
+        }
+
+        public static void SetAttach(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AttachProperty, value);
+        }
+
+        public static string GetPassword(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject obj, string value)
+        {
+            obj.SetValue(PasswordProperty, value);
+        }
+
+        private static void AttachPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                if (d is PasswordBox passwordBox)
+                {
+                    passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                }
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                SetPassword(passwordBox, passwordBox.Password);
+            }
+        }
+    }
+}
--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using PosApp.Services;
 using PosApp.Model;
+using System.Windows;
+using System.ComponentModel;
 
 namespace PosApp.ViewModel
 {
     public class LoginViewModel : ViewModelBase
     {
-        private readonly ServerSettingStore _serverSettingStore;
+        private ServerSettingStore _serverSettingStore;
 
         private string _username;
         private string _password;
@@ -34,8 +36,11 @@ namespace PosApp.ViewModel
             get { return _password; }
             set
             {
-                _password = value;
-                OnPropertyChanged(nameof(Password));
+                if (_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged(nameof(Password));
+                }
             }
 
         }
@@ -44,19 +49,26 @@ namespace PosApp.ViewModel
 
         public ICommand NavigateServerSettingCommand { get; }
         public ICommand LoginCommand { get; }
+        public ICommand LoadedLoginCommand { get; }
 
-        public LoginViewModel(ServerSettingStore serverSettingStore, 
+        public LoginViewModel(ServerSettingStore serverSettingStore,
             Func<NavigationBarViewModel> createNavigationBarViewModel,
             INavigationService dashboardNavigationService,
             INavigationService serverSettingNavigationService)
         {
             _serverSettingStore = serverSettingStore;
-
             NavigationBarViewModel = createNavigationBarViewModel();
 
             NavigateServerSettingCommand = new NavigateCommand(serverSettingNavigationService);
 
             LoginCommand = new LoginCommand(this, _serverSettingStore, dashboardNavigationService);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
